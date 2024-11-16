063575ec5892565e95c03cf8a75c8092c665b3b3
MikolajMarciniak <marciniak.miko@gmail.com>
2024-11-16 20:44:48 +0000

    modify run.sh

--- agorize_playground/CardinalityEstimation/run.sh
+++ agorize_playground/CardinalityEstimation/run.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-# Remove the 'build' directory if it exists
 if [ -d "build" ]; then
     rm -rf build
 fi
 
-# Create the 'build' directory
 mkdir build
 
-# Change into the 'build' directory
 cd build
 
-# Run cmake and make
 cmake ..
 make
 
-# Check if 'main' exists and execute it
 if [ -f "main" ]; then
     ./main
 else
